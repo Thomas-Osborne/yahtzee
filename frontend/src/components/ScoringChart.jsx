d4a8c9a282f6997d369bfbad954a7a0cd908a390
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-04-29 18:54:25 +0100

    Styled the scoring chart further

--- frontend/src/components/ScoringChart.jsx
+++ frontend/src/components/ScoringChart.jsx
@@ -2,8 +2,20 @@ export default function ScoringChart(props) {
 
   const rowElements = props.rows.map(row => (
     <tr key={row.name}>
-      <td className="border border-black px-4 py-2">{row.name}</td>
-      <td className={`border border-black px-4 py-2 ${row.isDisabled ? 'bg-gray-500' : 'bg-green-200'}`}>
+      <td className={
+        `px-4 py-2 border-black px-4 py-2
+         ${row.isCategory && row.isDisabled ? 'bg-gray-500' : 'bg-green-200'}
+         ${row.isCategory ? 'border border-solid' : ' border-y-2 border-l-2 bg-blue-300 text-2xl font-semibold'}
+         `}
+      >
+        {row.name}
+      </td>
+      <td className={
+        `px-4 py-2 border-black px-4 py-2
+         ${row.isCategory && row.isDisabled ? 'bg-gray-500' : 'bg-green-200'}
+         ${row.isCategory ? 'border border-solid' : ' border-y-2 border-r-2 bg-blue-300 text-2xl font-semibold'}
+         `}
+      >
         <button 
           disabled={row.isDisabled}
           onClick={() => props.chooseCategory(row)}
